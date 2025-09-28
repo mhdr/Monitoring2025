658d9dd6b99b4753b601e5b091f84f330421c7e3
Mahmood Ramzani <mahmoodramzani@gmail.com>
2025-09-28 18:40:23 +0330

    feat: enhance layout and responsiveness of multiple pages with flexbox and height adjustments

--- ui/src/components/ManagementPage.tsx
+++ ui/src/components/ManagementPage.tsx
@@ -5,14 +5,14 @@ const ManagementPage: React.FC = () => {
   const { t } = useLanguage();
 
   return (
-    <div className="container-fluid py-4">
-      <div className="row">
-        <div className="col-12">
-          <div className="card">
+    <div className="container-fluid h-100 d-flex flex-column py-4">
+      <div className="row flex-fill">
+        <div className="col-12 h-100">
+          <div className="card h-100 d-flex flex-column">
             <div className="card-header">
               <h4 className="card-title mb-0">{t('management')}</h4>
             </div>
-            <div className="card-body">
+            <div className="card-body flex-fill d-flex align-items-center justify-content-center">
               <p className="text-muted">
                 {t('management')} page content will be added here.
               </p>
