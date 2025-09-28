760fa3ed9fb2fcb15c434de3bbad25b49ae98d87
Mahmood Ramzani <mahmoodramzani@gmail.com>
2025-09-28 14:07:58 +0330

    feat: remove warehouse link from navbar and clean up navigation links section

--- ui/src/components/ResponsiveNavbar.tsx
+++ ui/src/components/ResponsiveNavbar.tsx
@@ -30,7 +30,7 @@ const ResponsiveNavbar = () => {
           href="#" 
           className="fw-bold fs-4 text-white navbar-brand-gradient"
         >
-          {t('monitoring')} {t('warehouse')}
+          {t('monitoring')}
         </Navbar.Brand>
         <Navbar.Toggle 
           aria-controls="basic-navbar-nav" 
@@ -38,30 +38,8 @@ const ResponsiveNavbar = () => {
         />
         <Navbar.Collapse id="basic-navbar-nav" className="custom-collapse">
           <Nav className="me-auto">
-            <Nav.Link 
-              href="#dashboard" 
-              className="text-white fw-medium px-3 py-2 rounded nav-link-hover me-1"
-            >
-              {t('dashboard')}
-            </Nav.Link>
-            <Nav.Link 
-              href="#warehouse" 
-              className="text-white fw-medium px-3 py-2 rounded nav-link-hover me-1"
-            >
-              {t('warehouse')}
-            </Nav.Link>
-            <Nav.Link 
-              href="#reports" 
-              className="text-white fw-medium px-3 py-2 rounded nav-link-hover me-1"
-            >
-              {t('reports')}
-            </Nav.Link>
-            <Nav.Link 
-              href="#settings" 
-              className="text-white fw-medium px-3 py-2 rounded nav-link-hover me-1"
-            >
-              {t('settings')}
-            </Nav.Link>
+                {/*TODO: Add navigation links here*/}
+                {/* Example Nav Links */}
           </Nav>
           
           {/* User Menu */}
