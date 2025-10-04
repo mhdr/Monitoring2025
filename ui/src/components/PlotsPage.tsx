92bf547365ea9444da2e63804b5476974e75bd38
Mahmood Ramzani <mahmoodramzani@gmail.com>
2025-10-04 10:55:13 +0330

    feat: add data attributes for improved accessibility in PlotsPage component

--- ui/src/components/PlotsPage.tsx
+++ ui/src/components/PlotsPage.tsx
@@ -5,15 +5,26 @@ const PlotsPage: React.FC = () => {
   const { t } = useLanguage();
 
   return (
-    <div className="container-fluid h-100 d-flex flex-column py-4">
-      <div className="row flex-fill">
-        <div className="col-12 h-100">
-          <div className="card h-100 d-flex flex-column">
-            <div className="card-header">
-              <h4 className="card-title mb-0">{t('plots')}</h4>
+    <div
+      className="container-fluid h-100 d-flex flex-column py-4"
+      data-id-ref="plots-page-container"
+    >
+      <div className="row flex-fill" data-id-ref="plots-page-row">
+        <div className="col-12 h-100" data-id-ref="plots-page-col">
+          <div
+            className="card h-100 d-flex flex-column"
+            data-id-ref="plots-page-card"
+          >
+            <div className="card-header" data-id-ref="plots-page-card-header">
+              <h4 className="card-title mb-0" data-id-ref="plots-page-title">
+                {t('plots')}
+              </h4>
             </div>
-            <div className="card-body flex-fill d-flex align-items-center justify-content-center">
-              <p className="text-muted">
+            <div
+              className="card-body flex-fill d-flex align-items-center justify-content-center"
+              data-id-ref="plots-page-card-body"
+            >
+              <p className="text-muted" data-id-ref="plots-page-placeholder">
                 {t('plots')} page content will be added here.
               </p>
             </div>
