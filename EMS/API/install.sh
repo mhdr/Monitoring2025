8f8fe962bc590d125d780e1588fc009e6af36d95
Mahmood Ramzani <mahmoodramzani@gmail.com>
2025-10-23 00:12:28 +0330

    feat: add systemd service and deployment script for EMS API

---
+++ EMS/API/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+deploy_dir=/opt/ems3/api
+service_name=ems3_api.service
+service_file=ems3_api.service
+
+# deploy
+sudo mkdir -p ${deploy_dir}
+sudo dotnet publish --output "${deploy_dir}" --configuration Release
+sudo cp run.sh ${deploy_dir}/run.sh
+
+# systemd
+sudo systemctl stop ${service_name}
+sudo systemctl disable ${service_name}
+sudo rm /etc/systemd/system/${service_file}
+sudo cp ${service_file} /etc/systemd/system/${service_file}
+sudo systemctl enable ${service_name}
