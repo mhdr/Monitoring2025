b65d42d143f9d2542a6c62cde26a949e2f2349f0
Mahmood Ramzani <mahmoodramzani@gmail.com>
2025-10-23 00:30:21 +0330

    Add script to convert and set permissions on .sh files
    Introduces dos2unix.sh, a script that converts all .sh files in the project to Unix line endings and sets executable permissions.

---
+++ dos2unix.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/bash
+
+find . -type f -name "*.sh" -exec dos2unix "{}" \;
+find . -type f -name "*.sh" -exec chmod +x "{}" \;
+# find . -type f -name "*.sh" -exec git update-index --chmod=+x "{}" \;
