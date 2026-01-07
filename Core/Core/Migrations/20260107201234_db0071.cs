3b6038a98807be4897421101d1d54387d02408da
Mahmood Ramzani <mahmoodramzani@gmail.com>
2026-01-07 23:44:11 +0330

    Add input_reference and input_type columns to statistical_memory table
    - Introduced a new column 'input_reference' of type text, which is non-nullable and has a default value of an empty string.
    - Added a new column 'input_type' of type integer, which is also non-nullable and defaults to 0.

---
+++ Core/Core/Migrations/20260107201234_db0071.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class db0071 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "input_reference",
+                table: "statistical_memory",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "input_type",
+                table: "statistical_memory",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "input_reference",
+                table: "statistical_memory");
+
+            migrationBuilder.DropColumn(
+                name: "input_type",
+                table: "statistical_memory");
+        }
+    }
+}
